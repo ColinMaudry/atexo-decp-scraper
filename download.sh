0f8dcf5ebf7eb72baa49bfa689966bb150dba86c
Colin Maudry <colin@maudry.com>
2020-01-15 16:23:28 +0100

    Plus d'infos dans les logs, script runnable sur X plateforme

--- download.sh
+++ download.sh
@@ -1,30 +1,56 @@
 #!/bin/bash
 
-if [[ ! -d xml ]]
+plateforme="$1"
+annee="$2"
+baseurl=""
+
+while IFS=, read -r id url
+do
+    if [[ "$id" == "$plateforme" ]]
+    then
+        baseurl="$url"
+        break;
+    fi
+done < plateformes.csv
+
+if [[ -z "$baseurl" ]]
 then
-    mkdir -p xml/vides
-    mkdir xml/html
+    echo "ID de plateforme $plateforme introuvable."
+    cat plateformes.csv
+    exit 1
 fi
 
-#for id in `jq -r '.[] | select(.disabled != true) | .id' megalis-acheteurs.json`
-for id in a5q a8z d3z i8h i8k i8l i8m i8n
-do
-    #nom=`jq -r '.[] | select(.id == "$id") | .name' megalis-acheteurs.json | sed -r 's/[ ,\x27]/-/g'`
-    for annee in 2019
-    do
-        curl -q "https://marches.megalisbretagne.org/app.php/api/v1/donnees-essentielles/contrat/xml-extraire-criteres/$id/0/1/$annee/false/false/false/false/false/false/false/false/false" > temp.xml
+xmldir="xml/$plateforme"
 
-        # Vérification que
-        # - le XML n'est pas vide
-        # - c'est bien du XML est retourné (et pas une page HTML)
-        if [[ `stat -c%s temp.xml` -lt 60 ]]
-        then
-            mv temp.xml xml/vides/${id}_${annee}.xml
-        elif [[ `head -c 5 temp.xml` == "<!DOC" ]]
-        then
-            mv temp.xml xml/html/${id}_${annee}.xml
-        else
-            mv temp.xml xml/${id}_${annee}.xml
-        fi
-    done
+if [[ ! -d "$xmldir" ]]
+then
+    mkdir -p $xmldir/vides
+    mkdir $xmldir/html
+fi
+
+for id in `jq -r '.[] | select(.disabled != true) | .id' acheteurs/${plateforme}.json`
+do
+    nom=`jq --arg id "$id" -r '.[] | select(.id == $id) | .name' acheteurs/${plateforme}.json | sed -r 's/[ ,\x27/]/-/g'`
+    echo "$nom ($id)"
+    curl "${baseurl}/app.php/api/v1/donnees-essentielles/contrat/xml-extraire-criteres/$id/0/1/$annee/false/false/false/false/false/false/false/false/false" > temp.xml 2> /dev/null
+
+    # Vérification que
+    # - le XML n'est pas vide
+    # - c'est bien du XML est retourné (et pas une page HTML)
+    if [[ `stat -c%s temp.xml` -lt 60 ]]
+    then
+        mv temp.xml "$xmldir/vides/${id}_${nom}_${annee}.xml"
+        echo "> vide"
+        echo ""
+    elif [[ `head -c 5 temp.xml` == "<!DOC" ]]
+    then
+        mv temp.xml "$xmldir/html/${id}_${nom}_${annee}.xml"
+        echo "> erreur"
+        echo ""
+    else
+        mv temp.xml "$xmldir/${id}_${nom}_${annee}.xml"
+        echo "> OK"
+        echo ""
+    fi
+
 done
