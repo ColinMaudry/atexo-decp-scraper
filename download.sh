bd6eff3f8d697b101315f38101efca3fdc2af9a4
Colin Maudry <colin@maudry.com>
2020-01-15 17:14:24 +0100

    Collecte de statistiques de diponibilité des données par acheteur

--- download.sh
+++ download.sh
@@ -28,32 +28,43 @@ then
  rm -r $xmldir
 fi
 
-for id in `jq -r '.[] | select(.disabled != true) | .id' acheteurs/${plateforme}.json`
+# Suppressiond des données de disponibilité pour la plateforme courante
+cat disponibilite-donnees.csv | grep -v "$plateforme," > temp.csv
+mv temp.csv disponibilite-donnees.csv
+
+mkdir -p $xmldir/vides
+mkdir $xmldir/html
+
+for id in `jq -r '.[] | .id' acheteurs/${plateforme}.json`
 do
     nom=`jq --arg id "$id" -r '.[] | select(.id == $id) | .name' acheteurs/${plateforme}.json | sed -r 's/[ ,\x27/]/-/g'`
     echo "$nom ($id)"
-    curl "${baseurl}/app.php/api/v1/donnees-essentielles/contrat/xml-extraire-criteres/$id/0/1/$annee/false/false/false/false/false/false/false/false/false" > temp.xml 2> /dev/null
+
+    url="${baseurl}/app.php/api/v1/donnees-essentielles/contrat/xml-extraire-criteres/$id/0/1/$annee/false/false/false/false/false/false/false/false/false"
+
+    tempxml=xml/temp.xml
+
+    date=`date +%Y-%m-%dT%H:%M:%S`
+
+    curl "$url" > $tempxml 2> /dev/null
 
     # Vérification que
     # - le XML n'est pas vide
-    # - c'est bien du XML est retourné (et pas une page HTML)
-    if [[ `stat -c%s temp.xml` -lt 60 ]]
+    if [[ `cat $tempxml | grep "<marche>" | wc -l` -eq 0 ]]
     then
-        mv temp.xml "$xmldir/vides/${id}_${nom}_${annee}.xml"
-        echo "> vide"
-        echo ""
-    elif [[ `head -c 5 temp.xml` == "<!DOC" ]]
+        mv $tempxml "$xmldir/vides/${id}_${nom}_${annee}.xml"
+        echo "$plateforme,\"$nom\",$annee,0,$date" >> disponibilite-donnees.csv
+    # - c'est bien du XML est retourné (et pas une page HTML (= page d'erreur))
+    elif [[ `head -c 5 $tempxml` == "<!DOC" ]]
     then
-        mv temp.xml "$xmldir/html/${id}_${nom}_${annee}.xml"
-        echo "> erreur"
-        echo ""
+        mv $tempxml "$xmldir/html/${id}_${nom}_${annee}.xml"
+        echo "$plateforme,\"$nom\",$annee,erreur,$date" >> disponibilite-donnees.csv
     else
-        mv temp.xml "$xmldir/${id}_${nom}_${annee}.xml"
-        echo "> OK"
-        echo ""
+        num=`cat $tempxml | grep "<marche>" | wc -l`
+        mv $tempxml "$xmldir/${id}_${nom}_${annee}.xml"
+        echo "$plateforme,\"$nom\",$annee,$num,$date" >> disponibilite-donnees.csv
     fi
-
-
+    
 done
 
 # Fusion des XML en un fichier
