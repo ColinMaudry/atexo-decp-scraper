4d0f2e52b80017e8ab82bb508611047dedd10ab1
Colin Maudry <colin@maudry.com>
2021-11-20 11:29:16 +0100

    Test api key

--- publish.sh
+++ publish.sh
@@ -12,7 +12,7 @@ export dataset_id="5f4d1921f7e627ef3ae26944"
 #API_KEY configurée dans les options de build de CircleCI
 api_key=$API_KEY
 echo ${api_key:0:10}
-exit 0
+exit 1
 
 echo "Mise à jour de $plateforme.xml..."
 
