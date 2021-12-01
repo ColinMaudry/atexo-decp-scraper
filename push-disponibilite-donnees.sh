5022266a7f1a525f72f298225f30f00b5b1536ae
Colin Maudry <colin@maudry.com>
2021-12-01 15:31:15 +0100

    Cleaner code

--- push-disponibilite-donnees.sh
+++ push-disponibilite-donnees.sh
@@ -3,10 +3,11 @@
 plateforme="$1"
 branch="$2"
 
-# To prevent clashes
 if [[ -f disponibilite-donnees-temp.csv ]]
 then
+  # To prevent clashes
   sleep $((1 + RANDOM % 30))
+
   git pull origin "$branch"
   cat disponibilite-donnees-temp.csv >> disponibilite-donnees.csv
   git commit -am "Update stats $plateforme"
